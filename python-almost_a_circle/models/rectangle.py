249e021462e56e14c2daec3529c823473eef8220
MMD <manuelmoralesdiaz0@gmail.com>
2024-02-16 19:22:48 -0400

    changing variables

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,12 +8,17 @@ class Rectangle(Base):
     """Defines a class Rectangle that inherits from Base."""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the class."""
-
+        
+        super().__init__()
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
+        if id is None:
+            self.id = Base().id
+        else:
+            self.id = id
+            Base().id = id
 
         @property
         def width(self):
