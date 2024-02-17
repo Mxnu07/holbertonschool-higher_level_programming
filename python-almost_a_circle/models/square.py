9061426c28f398640b16c23383af0f60bf8338f4
MMD <manuelmoralesdiaz0@gmail.com>
2024-02-17 18:33:28 -0400

    adding update def

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,25 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.width)
+    
+    def update(self, *args, **kwargs):
+        """Assigns value to attributes"""
+        if args is not None and len(args) is not 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for Key, value in kwargs.items():
+                if Key == "id":
+                    self.id = value
+                if Key == "size":
+                    self.size = value
+                if Key == "x":
+                    self.x = value
+                if Key == "y":
+                    self.y = value
