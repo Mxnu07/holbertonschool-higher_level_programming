d783a20c9f5287eff4b87ca54e4592c1552f7f8e
MMD <manuelmoralesdiaz0@gmail.com>
2024-02-17 18:37:24 -0400

    adding dictionary to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -50,3 +50,8 @@ class Square(Rectangle):
                     self.x = value
                 if Key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the square"""
+        SqrDict = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return SqrDict
