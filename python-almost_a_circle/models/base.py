4cb13a9d632e3b5db3ec8b638c38c7100a1e9033
MMD <manuelmoralesdiaz0@gmail.com>
2024-02-17 18:46:41 -0400

    adding json strings

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """This module defines a base class for future classes
  in this project."""
 
+import json
 
 class Base():
     """Defines a base class."""
@@ -14,3 +15,13 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return("[]")
+        if (type(list_dictionaries) is not list or
+                not all(type(X) == dict for X in list_dictionaries)):
+            return (json.dumps(list_dictionaries))
