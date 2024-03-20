8aa65b72dd446c4dd32c9c2c6c2704325715172f
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-19 21:43:29 -0400

    debugging

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""print all states from a database."""
+"""print all states from a database"""
 
 
-import MySQLdb
+import MySQLdb as sql
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost",
+    db = sql.connect(host="localhost",
                      port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
