73adaf9221a9b0305e79b78f70da3f866c794f89
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-21 22:05:54 -0400

    fix: removing query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
                      port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE BINARY name = '{}'".format(argv[4]))
-    rows = cur.fetchall(query)
+    rows = cur.fetchall()
     for row in rows:
         print(row)
