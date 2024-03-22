969adb1602aeeaa4587a39df27a55548e3d478af
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-21 21:53:41 -0400

    fix: changing string format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""print all states with a name starting with N (upper N) from the database"""
+"""script that takes in an argument and displays all values in the states table"""
 
 
 import MySQLdb as sql
@@ -7,13 +7,11 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    state_name = argv[4]
 
     db = sql.connect(host="localhost",
                      port=3306, user=argv[1], passwd=argv[2], db=argv[3], state_name=argv[4])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(argv[4])
     rows = cur.fetchall(query)
     for row in rows:
         print(row)
-
