d044544e0188bee03e4b388bbafed5d03a230751
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-21 21:59:15 -0400

    removin extra parentesis

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = sql.connect(host="localhost",
                      port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(argv[4]))
+    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(argv[4])
     rows = cur.fetchall(query)
     for row in rows:
         print(row)
