0e6623984d699a8b6a9af49cdfeabfff5b735ac6
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-21 21:08:13 -0400

    task 1 and 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""print all states with a name starting with N (upper N) from the database"""
+
+
+import MySQLdb as sql
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = sql.connect(host="localhost",
+                     port=3306, user=argv[1], passwd=argv[2], db=argv[3], state_name=argv[4])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    rows = cur.fetchall(query)
+    for row in rows:
+        print(row)
