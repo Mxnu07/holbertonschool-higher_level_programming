7dc01e39c06a467bf635e7fc58315ca01a57eeff
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-21 22:03:11 -0400

    fix: adding execute

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
                      port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     query = "SELECT * FROM states WHERE BINARY name = '{}'".format(argv[4])
+    cur.execute(query)
     rows = cur.fetchall(query)
     for row in rows:
         print(row)
